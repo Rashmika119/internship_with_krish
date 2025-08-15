f666988f8d72daec7213c5d3fe68b9cab78ddbcb
rashmika <rashmikanethsarani119@gmail.com>
2025-08-15 14:27:26 +0530

    handle the errors and completed the code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,40 +5,53 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Random random=new Random();
-        int number=random.nextInt(26);//0-25
+        int number=random.nextInt(26);
         char generatedLetter =(char)(number+'A');//A-Ascii value 65
 
         boolean isGuessCorrect=false;
         int count=0;
+        Scanner scan = new Scanner(System.in);
+        while(!isGuessCorrect) {
 
-        while(isGuessCorrect==false) {
             System.out.println("Guess the character: ");
-            Scanner scan = new Scanner(System.in);
-            char inputCharacter = scan.next().charAt(0);
-            char inputInUpperCase = Character.toUpperCase(inputCharacter);
-            int inputAsciiValue=inputInUpperCase;
+            try {
+                String inputCharacter = scan.nextLine().trim();
+                int arrayLength = inputCharacter.length();
 
-            if (inputInUpperCase == generatedLetter) {
-                count++;
-                isGuessCorrect=true;
-                System.out.println("You guessed the character " + inputCharacter+" ,after "+count+" trial. welldone!");
-            } else if ((inputAsciiValue<=(number+3+65)) && (inputAsciiValue>=(number-3+65))) {
-                count++;
-                System.out.println("Hot...You are So close..Try again!");
-                
-            }else if(((inputAsciiValue>=(number-6+65)&&(inputAsciiValue<=(number+6+65)))&& ((inputAsciiValue<(number-2+65)) &&(inputAsciiValue>(number+2+65))))) {
-                count++;
-                System.out.println("Warm...You are close..Try again!");
+                char userGuessedLetter = inputCharacter.charAt(0);
+                boolean isAlphabet = Character.isLetter(userGuessedLetter);
 
-            } else if (((inputAsciiValue>=(number-9+65)&&(inputAsciiValue<=(number+9+65)))&&((inputAsciiValue<(number-6+65)) &&(inputAsciiValue>(number+6+65))))) {
-                count++;
-                System.out.println("Cold...You are bit away the track..Try again!");
-            }else{
-                count++;
-                System.out.println("Ice...You are out of there..try again");
+                if (arrayLength == 1 && isAlphabet) {
+                    count++;
+                    char validUserGuessedLetter = inputCharacter.charAt(0);
+                    char inputInUpperCase = Character.toUpperCase(validUserGuessedLetter);
+
+                    int difference=Math.abs(inputInUpperCase-generatedLetter);
+
+                    if (difference == 0) {
+                        isGuessCorrect=true;
+                        System.out.println("You guessed the character " + generatedLetter + " ,after " + count + " trial. welldone!");
+                    } else if (difference <= 3) {
+                        System.out.println("Hot...You are So close..Try again!");
+
+                    } else if ((difference<=6)){
+                        System.out.println("Warm...You are close..Try again!");
+
+                    } else if ((difference<=9) ) {
+                        System.out.println("Cold...You are bit away the track..Try again!");
+                    } else {
+                        System.out.println("Ice...You are out of there..try again");
+                    }
+
+                } else {
+                    System.out.println("Invalid input.Enter a valid input character");
+                }
+            }catch (Exception e) {
+                System.out.println("Invalid input...message: "+e.getMessage());
             }
+        }
         }
 
     }
-}
